7fd8df4ec724624cd3e89102ee485a049e419b3d
Foretack <69414142+Foretack@users.noreply.github.com>
2024-01-15 08:50:52 +0100

    Add a shit ton more of nightwave challenges

--- src/parsing/sys.zig
+++ src/parsing/sys.zig
@@ -33,43 +33,127 @@ pub fn rivenSliverPickup(log: log_types.LogEntry) bool {
 }
 
 pub fn nightwaveChallengeComplete(log: log_types.LogEntry) ?game_types.NightwaveChallenge {
-    if (std.mem.startsWith(u8, log.message, ": LotusProfileData::NotifyWorldStateChallengeCompleted")) {
-        var tier: game_types.ChallengeTier = undefined;
-        if (std.mem.indexOf(u8, log.message, "/WeeklyHard/") != null) {
-            tier = .EliteWeekly;
-        } else if (std.mem.indexOf(u8, log.message, "/Weekly/") != null) {
-            tier = .Weekly;
-        } else {
-            tier = .Daily;
-        }
-
-        const last_slash_idx = std.mem.lastIndexOf(u8, log.message, "/") orelse return null;
-        var challenge_name: []const u8 = undefined;
-        const challenge = std.meta.stringToEnum(game_types.NightwaveChallenges, log.message[last_slash_idx + 1 ..]) orelse .UNKNOWN;
-        challenge_name = switch (challenge) {
-            .SeasonWeeklyHardFriendsProfitTaker => "Kill the Profit-Taker in Orb Vallis",
-            .SeasonWeeklyHardRailjackMissions => "Complete 8 Railjack missions",
-            .SeasonWeeklyCompleteSpy => "Complete 3 spy missions",
-            .SeasonWeeklyRailjackHijackDestroyThree => "While piloting a hijacked crewship, destroy 3 enemy fighters",
-            .SeasonWeeklyPermanentKillEnemies7 => "Kill 500 enemies",
-            .SeasonWeeklyPermanentKillEximus7 => "Kill 30 eximus enemies",
-            .SeasonWeeklyPermanentCompleteMissions7 => "Complete any 15 missions",
-            .SeasonDailyPickUpMods => "Pick up 8 modules",
-            .SeasonDailyVisitFeaturedDojo => "Visit a featured dojo",
-            .SeasonDailyKillEnemiesWithMelee => "Kill 150 enemies with a melee weapon",
-            .SeasonWeeklyLoyalty => "Interact with your Kubrow or Kavat",
-            .SeasonDailyKillEnemiesWithRadiation => "Kill 150 enemies with radiation damage",
-            .SeasonDailyDeploySpecter => "Deploy a Specter",
-            .SeasonDailyTwoForOne => "Pierce and kill 2 or more enemies in a single bow shot",
-            .SeasonDailyCompleteMission => "Complete a mission",
-            else => log.message[last_slash_idx + 1 ..],
-        };
-
-        return game_types.NightwaveChallenge{
-            .name = challenge_name,
-            .tier = tier,
-        };
+    if (!std.mem.startsWith(u8, log.message, ": LotusProfileData::NotifyWorldStateChallengeCompleted")) {
+        return null;
     }
 
-    return null;
+    var tier: game_types.ChallengeTier = undefined;
+    if (std.mem.indexOf(u8, log.message, "/WeeklyHard/") != null) {
+        tier = .EliteWeekly;
+    } else if (std.mem.indexOf(u8, log.message, "/Weekly/") != null) {
+        tier = .Weekly;
+    } else {
+        tier = .Daily;
+    }
+
+    const last_slash_idx = std.mem.lastIndexOf(u8, log.message, "/") orelse return null;
+    var challenge_name: []const u8 = undefined;
+    @setEvalBranchQuota(3000);
+    const challenge = std.meta.stringToEnum(game_types.NightwaveChallenges, log.message[last_slash_idx + 1 ..]) orelse .UNKNOWN;
+    challenge_name = switch (challenge) {
+        .SeasonDailyPickUpMods => "Pick up 8 modules",
+        .SeasonDailyVisitFeaturedDojo => "Visit a featured dojo",
+        .SeasonDailyDeploySpecter => "Deploy a Specter",
+        .SeasonDailyKillEnemiesWithMelee => "Kill 150 enemies with a melee weapon",
+        .SeasonDailyKillEnemiesWithRadiation => "Kill 150 enemies with radiation damage",
+        .SeasonDailyTwoForOne => "Pierce and kill 2 or more enemies in a single bow shot",
+        .SeasonDailyCompleteMission => "Complete a mission",
+        .SeasonDailyTransmuteMods => "Complete 3 transmutations",
+        .SeasonDailyKillEnemiesWithHeadshots => "Kill 40 enemies with headshots",
+        .SeasonDailySolveCiphers => "Hack 8 Consoles",
+        .SeasonDailySlideKills => "Kill 20 enemies while sliding",
+        .SeasonDailyPlayEmote => "Play 1 Emote",
+        .SeasonDailyPlaceMarker => "Mark 5 mods or resources",
+        .SeasonDailyPickUpEnergy => "Pick up 20 energy orbs",
+        .SeasonDailyOpenLockers => "Open 20 lockers",
+        .SeasonDailyKillEnemiesWithFire => "Kill 150 enemies with heat damage",
+        .SeasonDailyKillEnemiesWithViral => "Kill 150 enemies with viral damage",
+        .SeasonDailyKillEnemiesWithSecondary => "Kill 150 enemies with a Secondary Weapon",
+        .SeasonDailyKillEnemiesWithPoison => "Kill 150 enemies with toxin damage",
+        .SeasonDailyKillEnemiesWithPrimary => "Kill 150 enemies with a Primary Weapon",
+        .SeasonDailyKillEnemiesWithMagnetic => "Kill 150 enemies with magnetic damage",
+        .SeasonDailyKillEnemiesWithGas => "Kill 150 enemies with gas damage",
+        .SeasonDailyKillEnemiesWithFreeze => "Kill 150 enemies with cold damage",
+        .SeasonDailyKillEnemiesWithFinishers => "Kill 10 enemies with finishers",
+        .SeasonDailyKillEnemiesWithElectricity => "Kill 150 enemies with electric damage",
+        .SeasonDailyKillEnemiesWithCorrosive => "Kill 150 enemies with corrosive damage",
+        .SeasonDailyKillEnemiesWithBlast => "Kill 150 enemies with blast damage",
+        .SeasonDailyKillEnemiesWithAbilities => "Kill 150 enemies with abilities",
+        .SeasonDailyPickUpMedallion => "Find 5 syndicate medallions",
+        .SeasonDailyKillEnemies => "Kill 200 enemies",
+        .SeasonDailyInteractWithPet => "Interact with your kubrow or kavat",
+        .SeasonDailyDeployGlyph => "Deploy a glyph in a mission",
+        .SeasonDailyDeployAirSupport => "Deploy an air support charge in a mission",
+        .SeasonDailyCollectCredits => "Pick up 15000 credits",
+        .SeasonDailyCodexScan => "Scan 25 objects or enemies",
+        .SeasonDailyBulletJump => "Bullet jump 150 times",
+        .SeasonDailyAimGlide => "Kill 20 enemies while aim gliding",
+
+        .SeasonWeeklyLoyalty => "Interact with your Kubrow or Kavat",
+        .SeasonWeeklyCompleteSpy => "Complete 3 spy missions",
+        .SeasonWeeklyRailjackHijackDestroyThree => "While piloting a hijacked crewship, destroy 3 enemy fighters",
+        .SeasonWeeklyVenusBounties => "Complete 5 different Bounties in Orb Vallis",
+        .SeasonWeeklyPlainsBounties => "Complete 5 different Bounties in the Plains of Eidolon",
+        .SeasonWeeklyUseForma => "Polarize a weapon, companion, or warframe",
+        .SeasonWeeklyUnlockRelics => "Unlock 3 relics",
+        .SeasonWeeklyUnlockDragonVaults => "Unlock 4 orokin vaults",
+        .SeasonWeeklySimarisScan => "Complete 5 scans for cephalon simaris",
+        .SeasonWeeklySanctuaryOnslaught => "Complete 8 waves of sanctuary onslaught",
+        .SeasonWeeklySabotageCaches => "Find all caches in 3 sabotage missions",
+        .SeasonWeeklyPickUpRareMods => "Pick up 8 Rare Mods",
+        .SeasonWeeklyPerfectAnimalCapture => "Complete 6 different perfect animal captures in Orb Vallis",
+        .SeasonWeeklyMineRareVenusResources => "Mine 6 rare gems in Orb Vallis",
+        .SeasonWeeklyMineRarePlainsResources => "Mine 6 Rare Gems or Ore in the Plains of Eidolon",
+        .SeasonWeeklyKillThumper => "Kill a Tusk Thumper in the Plains of Eidolon",
+        .SeasonWeeklyKillEximus => "Kill 30 Eximus",
+        .SeasonWeeklyKillEnemies => "Kill 500 enemies",
+        .SeasonWeeklyGildModular => "Gild 1 modular item",
+        .SeasonWeeklyCompleteVenusRace => "Complete 3 different K-Drive races in Orb Vallis",
+        .SeasonWeeklyCompleteTreasures => "Fully socket 3 ayatan sculptures",
+        .SeasonWeeklyCompleteSyndicateMissions => "Complete 10 syndicate missions",
+        .SeasonWeeklyCompleteSortie => "Complete 1 Sortie",
+        .SeasonWeeklyCompleteSabotage => "Complete 3 sabotage missions",
+        .SeasonWeeklyCompleteRescue => "Complete 3 rescue missions",
+        .SeasonWeeklyCompleteNightmareMissions => "Complete 3 nightmare missions of any type",
+        .SeasonWeeklyCompleteMobileDefense => "Complete 3 mobile defense missions",
+        .SeasonWeeklyCompleteKuva => "Complete 3 kuva siphon missions",
+        .SeasonWeeklyCompleteInvasionMissions => "Complete 9 invasion missions of any type",
+        .SeasonWeeklyCompleteExterminate => "Complete 3 exterminate missions",
+        .SeasonWeeklyCompleteDisruptionConduits => "Complete 12 conduits in disruption",
+        .SeasonWeeklyCompleteClemMission => "Help Clem with his weekly mission",
+        .SeasonWeeklyCompleteCapture => "Complete 3 capture missions",
+        .SeasonWeeklyCompleteAssassination => "Complete 3 Assassination missions",
+        .SeasonWeeklyCatchRareVenusFish => "Catch 6 rare servofish in Orb Vallis",
+        .SeasonWeeklyCatchRarePlainsFish => "Catch 6 rare fish in the Plains of Eidolon",
+
+        .SeasonWeeklyPermanentCompleteMissions7 => "Complete any 15 missions",
+        .SeasonWeeklyPermanentKillEnemies7 => "Kill 500 enemies",
+        .SeasonWeeklyPermanentKillEximus7 => "Kill 30 eximus enemies",
+
+        .SeasonWeeklyHardRailjackMissions => "Complete 8 Railjack missions",
+        .SeasonWeeklyHardFriendsProfitTaker => "Kill the Profit-Taker in Orb Vallis",
+        .SeasonWeeklyHardUnlockRelics => "Unlock 10 relics",
+        .SeasonWeeklyHardLuaPuzzles => "Complete 4 halls of acension on Lua",
+        .SeasonWeeklyHardKuvaSurvivalNoCapsules => "Survive for over 30 minutes in kuva survival",
+        .SeasonWeeklyHardKillThumper => "Kill a Tusk Thumper Doma in the Plains of Eidolon",
+        .SeasonWeeklyHardKillSilverGroveSpecters => "Kill 3 Silver Grove Specters",
+        .SeasonWeeklyHardKillRopalyst => "Defeat the Ropalyst",
+        .SeasonWeeklyHardKillEximus => " Kill 100 eximus",
+        .SeasonWeeklyHardKillEnemies => "Kill 1500 enemies",
+        .SeasonWeeklyHardIndexWinStreak => "Win 3 wagers in a row without letting the enemy score in one match of The Index",
+        .SeasonWeeklyHardFriendsSurvival => "Complete a survival mission reaching at least 30 minutes",
+        .SeasonWeeklyHardFriendsDefense => "Complete a defense mission reaching at least wave 20",
+        .SeasonWeeklyHardExterminateNoAlarm => "Complete an Extermination Mission with level 30 or higher enemies without being detected",
+        .SeasonWeeklyHardEliteSanctuaryOnslaught => "Complete 8 zones of Elite Sanctuary Onslaught",
+        .SeasonWeeklyHardKillOrCaptureRainalyst => "Kill or Capture an Eidolon Hydrolyst",
+        .SeasonWeeklyHardCompleteSortie => "Complete 3 sorties",
+        .SeasonWeeklyHardCompleteNightmareMissions => "Complete 10 nightmare missions of any type",
+
+        else => log.message[last_slash_idx + 1 ..],
+    };
+
+    return game_types.NightwaveChallenge{
+        .name = challenge_name,
+        .tier = tier,
+    };
 }
